11effc1b4950cd21a9f5476d053fd095799edabf
Richard Tier <rihard@highertier.com>
2017-03-28 10:08:41 +0100

    Udate the supplier company url

--- tests/test_buyer.py
+++ tests/test_buyer.py
@@ -8,9 +8,9 @@ class BuyerAPIClientTest(TestCase):
 
     def setUp(self):
         self.client = BuyerAPIClient(
-            base_url='https://buyer.com', api_key='test'
+            base_url='http://b.co/', api_key='test'
         )
 
-    @stub_request('https://buyer.com/api/external/company/supplier/1/', 'get')
+    @stub_request('http://b.co/company/supplier/1/company/', 'get')
     def test_retrieve_supplier_company(self, stub):
         self.client.retrieve_supplier_company(1)
